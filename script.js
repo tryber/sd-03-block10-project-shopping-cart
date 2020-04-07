a09fac8d456fdea14f3ae9506abb3683e54e6271
Gabriel Lucas <dreadpool38@gmail.com>
2020-04-07 18:46:59 -0300

    mais um codeclimate

--- script.js
+++ script.js
@@ -7,9 +7,7 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
-function cartItemClickListener() {
-
-}
+function cartItemClickListener() {}
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
@@ -20,8 +18,8 @@ function createCartItemElement({ sku, name, salePrice }) {
 
 function addToCart(ItemID) {
   fetch(`https://api.mercadolibre.com/items/${ItemID}`)
-    .then((e) => e.json())
-    .then((e) => {
+    .then(e => e.json())
+    .then(e => {
       const objeto = { sku: [e.id], name: [e.title], salePrice: [e.price] };
       const cartElement = createCartItemElement(objeto);
       document.querySelector('.cart__items').appendChild(cartElement);
@@ -31,7 +29,9 @@ function addToCart(ItemID) {
 function addEvents() {
   const buttons = document.getElementsByClassName('item__add');
   for (let i = 0; i < buttons.length; i += 1) {
-    buttons[i].addEventListener('click', (e) => addToCart(e.target.parentNode.querySelector('.item__sku').innerText));
+    buttons[i].addEventListener('click', e =>
+      addToCart(e.target.parentNode.querySelector('.item__sku').innerText)
+    );
   }
 }
 
@@ -50,7 +50,7 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(
-    createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'),
+    createCustomElement('button', 'item__add', 'Adicionar ao carrinho!')
   );
 
   return section;
@@ -60,18 +60,19 @@ function createProductItemElement({ sku, name, image }) {
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
-
 const itens = document.querySelector('.items');
 fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
-  .then((result) => result.json())
-  .then((result) => result.results)
-  .then((result) => result.forEach((e) => {
-    const elementObject = {
-      sku: [e.id],
-      name: [e.title],
-      image: [e.thumbnail],
-    };
-    const elemento = createProductItemElement(elementObject);
-    itens.appendChild(elemento);
-  }))
+  .then(result => result.json())
+  .then(result => result.results)
+  .then(result =>
+    result.forEach(e => {
+      const elementObject = {
+        sku: [e.id],
+        name: [e.title],
+        image: [e.thumbnail],
+      };
+      const elemento = createProductItemElement(elementObject);
+      itens.appendChild(elemento);
+    })
+  )
   .then(addEvents);
