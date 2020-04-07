b1c5410f0cbe9d1d345a0596e67c140f3f0d7440
Gabriel Lucas <dreadpool38@gmail.com>
2020-04-07 17:15:28 -0300

    codeclimate

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-window.onload = function onload() { };
+window.onload = function onload() {};
 
 function createProductImageElement(imageSource) {
-  const img = document.createElement('img');
-  img.className = 'item__image';
+  const img = document.createElement("img");
+  img.className = "item__image";
   img.src = imageSource;
   return img;
 }
@@ -15,19 +15,21 @@ function createCustomElement(element, className, innerText) {
 }
 
 function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
+  const section = document.createElement("section");
+  section.className = "item";
 
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createCustomElement("span", "item__sku", sku));
+  section.appendChild(createCustomElement("span", "item__title", name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(
+    createCustomElement("button", "item__add", "Adicionar ao carrinho!")
+  );
 
   return section;
 }
 
 function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
+  return item.querySelector("span.item__sku").innerText;
 }
 
 function cartItemClickListener(event) {
@@ -35,46 +37,45 @@ function cartItemClickListener(event) {
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
+  const li = document.createElement("li");
+  li.className = "cart__item";
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  li.addEventListener("click", cartItemClickListener);
   return li;
 }
 let productList;
-let itens = document.querySelector('.items')
+let itens = document.querySelector(".items");
 fetch("https://api.mercadolibre.com/sites/MLB/search?q=computador")
-  .then(result => result.json())
-  .then(result => result.results)
-  .then(result => result.forEach(e => {
-    let elementObject = {sku:[e.id],name:[e.title],image:[e.thumbnail]}
-   let elemento =  createProductItemElement(elementObject)
-   itens.appendChild(elemento)
-
-  }))
-  .then(addEvents)
-
-  function addToCart(ItemID){
-
-    fetch(`https://api.mercadolibre.com/items/${ItemID}`)
-    .then(e => e.json())
-    .then(e => {
-     
-     let objeto = {sku:[e.id],name:[e.title],salePrice:[e.price]}
-     let cartElement = createCartItemElement(objeto)
-     document.querySelector('.cart__items').appendChild(cartElement)
+  .then((result) => result.json())
+  .then((result) => result.results)
+  .then((result) =>
+    result.forEach((e) => {
+      let elementObject = {
+        sku: [e.id],
+        name: [e.title],
+        image: [e.thumbnail],
+      };
+      let elemento = createProductItemElement(elementObject);
+      itens.appendChild(elemento);
     })
+  )
+  .then(addEvents);
+
+function addToCart(ItemID) {
+  fetch(`https://api.mercadolibre.com/items/${ItemID}`)
+    .then((e) => e.json())
+    .then((e) => {
+      let objeto = { sku: [e.id], name: [e.title], salePrice: [e.price] };
+      let cartElement = createCartItemElement(objeto);
+      document.querySelector(".cart__items").appendChild(cartElement);
+    });
+}
+
+function addEvents() {
+  let buttons = document.getElementsByClassName("item__add");
+  for (let i = 0; i < buttons.length; i += 1) {
+    buttons[i].addEventListener("click", (e) =>
+      addToCart(e.target.parentNode.querySelector(".item__sku").innerText)
+    );
   }
-
-
-
-
-  function addEvents(){
-    let buttons = document.getElementsByClassName('item__add')
-    for (let i=0;i < buttons.length;i += 1){
-      buttons[i].addEventListener('click',(e) => addToCart(e.target.parentNode.querySelector('.item__sku').innerText))
-    }
-
-  }
-
-
+}
