0d474d633d06e88acec8b1163f59e7fdc37d432e
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-04-08 11:09:48 -0300

    [ANA CAROLINA] Shopping Cart Project

--- script.js
+++ script.js
@@ -1,43 +1,55 @@
-window.onload = function onload() { };
-
-function createProductImageElement(imageSource) {
+const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
-}
+};
 
-function createCustomElement(element, className, innerText) {
+const createCustomElement = (element, className, innerText) => {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
-}
+};
 
-function createProductItemElement({ sku, name, image }) {
+const createProductItemElement = ({ sku, name, image }) => {
   const section = document.createElement('section');
   section.className = 'item';
 
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(
+    createCustomElement('button', 'item__add', 'Adicionar ao carrinho!')
+  );
 
   return section;
-}
+};
 
-function getSkuFromProductItem(item) {
+const getSkuFromProductItem = (item) => {
   return item.querySelector('span.item__sku').innerText;
-}
+};
 
-function cartItemClickListener(event) {
+const cartItemClickListener = (event) => {
   // coloque seu código aqui
-}
+};
 
-function createCartItemElement({ sku, name, salePrice }) {
+const createCartItemElement = ({ sku, name, salePrice }) => {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-}
+};
+
+const API = (url) => fetch(url).then((response) => response.json());
+
+window.onload = async () => {
+  await API(
+    'https://api.mercadolibre.com/sites/MLB/search?q=$computador'
+  ).then((data) =>
+    data.results.forEach(({ id, title, thumbnail }) =>
+      createProductItemElement(id, title, thumbnail)
+    )
+  );
+};
