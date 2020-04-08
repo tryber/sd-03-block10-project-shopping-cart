8b8563f8c011dd0fe88083869f8523f8f00bdc64
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-04-08 17:47:48 -0300

    exclui o code

--- script.js
+++ script.js
@@ -68,5 +68,5 @@ window.onload = () => {
   document.getElementsByClassName('empty-cart')[0].addEventListener('click', () => {
     document.getElementsByClassName('cart__items')[0].innerHTML = '';
     update();
-  })
+  });
 };
