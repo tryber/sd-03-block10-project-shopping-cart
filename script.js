6a7d73becb23abddeeda95f7dc7888e107af8630
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-04-08 10:58:16 -0300

    Finishing code

--- script.js
+++ script.js
@@ -14,12 +14,25 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+const sumPrices = () => {
+  const cartItem = document.querySelectorAll('.cart__item');
+  document.getElementsByClassName('total-price')[0].textContent = Math.round([...cartItem].map(e => e.textContent
+    .match(/([0-9.]){1,}$/))
+    .reduce((acc, price) => acc + parseFloat(price), 0) * 100) / 100;
+};
+
+const updateCart = () => {
+  localStorage.setItem('itemCart', document.getElementsByClassName('cart__items')[0].innerHTML);
+  sumPrices();
+};
+
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
 function cartItemClickListener(event) {
   event.target.remove();
+  updateCart();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -39,14 +52,13 @@ const addElementToCart = async ({ sku }) => {
       name: product.title,
       salePrice: product.price,
     }));
+    updateCart();
   });
-  await (localStorage.setItem('itemCart', document.getElementsByClassName('cart__items')[0].innerHTML));
 };
 
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
@@ -56,8 +68,8 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function apiCreateItem() {
-  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador', myObject)
+async function apiCreateItem() {
+  await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador', myObject)
   .then(response => response.json())
   .then((data) => {
     const items = document.querySelector('.items');
@@ -72,14 +84,18 @@ function apiCreateItem() {
         ));
     });
   });
+  document.getElementsByClassName('loading')[0].remove();
 }
 
 window.onload = async function onload() {
   await apiCreateItem();
   document.getElementsByClassName('empty-cart')[0].addEventListener('click', () => {
     localStorage.setItem('itemCart', '');
+    localStorage.setItem('cartTotalPrice', 0);
     document.getElementsByClassName('cart__items')[0].innerHTML = '';
+    document.getElementsByClassName('total-price')[0].innerHTML = 0;
   });
   document.getElementsByClassName('cart__items')[0].innerHTML = localStorage.getItem('itemCart');
   document.querySelectorAll('li').forEach(li => li.addEventListener('click', cartItemClickListener));
+  await sumPrices();
 };
