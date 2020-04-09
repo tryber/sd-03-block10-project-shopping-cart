cbdec51764e9b19720d7e05c2bc34336a4275ec4
wendersonbelko <contato@wendersonbelko.com>
2020-04-09 15:23:03 -0300

    Tentando resolver os 800 erros do code climate

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 update = () => {
-  localStorage.setItem(
-    "Lista Salva",
-    document.getElementsByClassName("cart__items")[0].innerHTML
-  );
-  localStorage.setItem(
-    "Total a Pagar",
-    document.getElementsByClassName("total-price")[0].innerHTML
-  );
+  localStorage.setItem('Lista Salva', document.getElementsByClassName('cart__items')[0].innerHTML);
+  localStorage.setItem('Total a Pagar', document.getElementsByClassName('total-price')[0].innerHTML);
 };
 
 function createProductImageElement(imageSource) {
-  const img = document.createElement("img");
-  img.className = "item__image";
+  const img = document.createElement('img');
+  img.className = 'item__image';
   img.src = imageSource;
   return img;
 }
@@ -24,21 +18,18 @@ function createCustomElement(element, className, innerText) {
 }
 
 function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement("section");
-  section.className = "item";
-  section.appendChild(createCustomElement("span", "item__sku", sku));
-  section.appendChild(createCustomElement("span", "item__title", name));
+  const section = document.createElement('section');
+  section.className = 'item';
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section
-    .appendChild(
-      createCustomElement("button", "item__add", "Adicionar ao carrinho!")
-    )
-    .addEventListener("click", () => addToCart(sku));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'))
+          .addEventListener('click', () => addToCart(sku));
   return section;
 }
 
 function getSkuFromProductItem(item) {
-  return item.querySelector("span.item__sku").innerText;
+  return item.querySelector('span.item__sku').innerText;
 }
 
 cartItemClickListener = async (event) => {
@@ -48,26 +39,23 @@ cartItemClickListener = async (event) => {
 };
 
 function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement("li");
-  li.className = "cart__item";
+  const li = document.createElement('li');
+  li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener("click", () => cartItemClickListener(li));
+  li.addEventListener('click', () => cartItemClickListener(li));
   cardTotal(update());
   return li;
 }
 
 cardTotal = () => {
-  const cartItem = document.querySelectorAll(".cart__item");
-  const price =
-    Math.round(
-      [...cartItem]
-        .map((e) => e.textContent.match(/([0-9.]){1,}$/))
-        .reduce((acc, priceItem) => acc + parseFloat(priceItem), 0) * 100
-    ) / 100;
-  document.getElementsByClassName("total-price")[0].innerHTML = `${price}`;
+  const cartItem = document.querySelectorAll('.cart__item');
+  const price = Math.round([...cartItem].map(e => e.textContent
+  .match(/([0-9.]){1,}$/))
+  .reduce((acc, priceItem) => acc + parseFloat(priceItem), 0) * 100) / 100;
+  document.getElementsByClassName('total-price')[0].innerHTML = `${price}`;
 };
 
-const DontRepeat = (add) => ({
+const DontRepeat = add => ({
   sku: add.id,
   name: add.title,
   salePrice: add.price,
@@ -76,44 +64,25 @@ const DontRepeat = (add) => ({
 
 addToCart = async (sku) => {
   await fetch(`https://api.mercadolibre.com/items/${sku}`)
-    .then((response) => response.json())
-    .then((add) =>
-      document
-        .getElementsByClassName("cart__items")[0]
-        .appendChild(createCartItemElement(DontRepeat(add)))
-    );
+  .then(response => response.json())
+  .then(add => document.getElementsByClassName('cart__items')[0].appendChild(createCartItemElement(DontRepeat(add))));
   await cardTotal();
   await update();
 };
 
-disableLoad = () => document.getElementsByClassName("loading")[0].remove();
+disableLoad = () => document.getElementsByClassName('loading')[0].remove();
 
 window.onload = async () => {
-  await fetch("https://api.mercadolibre.com/sites/MLB/search?q=computador")
-    .then((response) => response.json())
-    .then((obj) =>
-      obj.results.map((e) =>
-        document
-          .getElementsByClassName("items")[0]
-          .appendChild(createProductItemElement(DontRepeat(e)))
-      )
-    );
-  document.getElementsByClassName(
-    "cart__items"
-  )[0].innerHTML = localStorage.getItem("Lista Salva");
-  document.getElementsByClassName(
-    "total-price"
-  )[0].innerHTML = localStorage.getItem("Total a Pagar");
-  document
-    .querySelectorAll("li")
-    .forEach((inner) =>
-      inner.addEventListener("click", () => cartItemClickListener(inner))
-    );
-  document
-    .getElementsByClassName("empty-cart")[0]
-    .addEventListener("click", () => {
-      document.getElementsByClassName("cart__items")[0].innerHTML = "";
-    });
+  await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+    .then(response => response.json())
+    .then(obj => obj.results.map(e => document.getElementsByClassName('items')[0]
+          .appendChild(createProductItemElement(DontRepeat(e)))));
+  document.getElementsByClassName('cart__items')[0].innerHTML = localStorage.getItem('Lista Salva');
+  document.getElementsByClassName('total-price')[0].innerHTML = localStorage.getItem('Total a Pagar');
+  document.querySelectorAll('li').forEach(inner => inner.addEventListener('click', () => cartItemClickListener(inner)));
+  document.getElementsByClassName('empty-cart')[0].addEventListener('click', () => {
+    document.getElementsByClassName('cart__items')[0].innerHTML = '';
+  });
   await disableLoad();
   await cardTotal();
   await update();
