67c30629d3b5f1438f6e3b129f9178605c746dfb
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-04-09 18:19:49 -0300

    Codeclimate check

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const sumAll = async () => {
   const sumPrices = () =>
     ([...allItems]
       .map(e => e.textContent.match(/([0-9.]){1,}$/))
-      .reduce((acc, price) => acc + parseFloat(price), 0) *
+      .reduce((acc, price) => acc + parseFloat(price), 0).toFixed(2) *
       100) /
     100;
   totalPriceClass.innerHTML = sumPrices();
