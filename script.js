5e9ca2cb3a4a7d2f35a47e7037bef3789f1edab7
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-04-09 17:35:20 -0300

    Função que soma os valores e mostra realtime na tela

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const addElement = (className, callback, obj) =>
   document.getElementsByClassName(className)[0].appendChild(callback(obj));
 
+/* Códigos referentes ao carrinho */
+
 const updateCart = () =>
   localStorage.setItem('Cart_items', document.getElementsByClassName('cart__items')[0].innerHTML);
 
 function cartItemClickListener(event) {
   event.target.remove();
   updateCart();
+  sumAll();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -27,8 +30,11 @@ const addToCart = async ({ sku }) => {
         salePrice: product.price,
       }),
     );
-  await updateCart();
+  updateCart();
+  sumAll();
 };
+
+/* Código referente à criação dos Elementos dos produtos */
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -51,26 +57,30 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  const btnAddToCart = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
-  // tentar salvar
-  btnAddToCart.addEventListener('click', () => {
+  // Mudando aqui para que todo elemento criado tenha setado um tipo - button -
+  //com classname - item__add - com texto Adicionar ao carrinho!
+  const addToCartButton = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  // Depois, adicionando um evento de click, para adicionar ao carrinho o ID da mercadoria clicada.
+  addToCartButton.addEventListener('click', () => {
     addToCart({ sku });
   });
-  section.appendChild(btnAddToCart);
-  //
+  // Depois, falamos pra section apender esse novo item - addToCartButton -
+  section.appendChild(addToCartButton);
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+/* ______________________________________________________  */
 
-/* ____________________________ MY CODE ___________________________  */
-
-/*
-const addElement = (className, callback, obj) =>
-  document.getElementsByClassName(className)[0].appendChild(callback(obj));
- */
+const sumAll = async () => {
+  const allItems = document.getElementsByClassName('cart__item');
+  const totalPriceClass = document.querySelector('.total-price');
+  const sumPrices = () =>
+    [...allItems]
+      .map(e => e.textContent.match(/([0-9.]){1,}$/))
+      .reduce((a, b) => Math.round(a + parseFloat(b)), 0 * 100)
+      .toFixed(2);
+      totalPriceClass.innerHTML = `TOTAL: $${sumPrices()}`;
+};
 
 window.onload = async () => {
   await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
@@ -84,10 +94,16 @@ window.onload = async () => {
         }),
       ),
     );
-  document.getElementsByClassName('cart__items')[0].innerHTML = localStorage.getItem('Cart-items');
   const esvaziaCarrinho = document.getElementsByClassName('empty-cart')[0];
   esvaziaCarrinho.addEventListener('click', () => {
     const clearOl = document.getElementsByClassName('cart__items')[0];
     clearOl.innerHTML = '';
+    updateCart();
+    sumAll();
   });
+  document.getElementsByClassName('cart__items')[0].innerHTML = localStorage.getItem('Cart_items');
+  document
+    .querySelectorAll('li')
+    .forEach(li => li.addEventListener('click', cartItemClickListener));
+  await sumAll();
 };
