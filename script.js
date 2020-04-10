e421dbb3c096034e0b5e8794d28c3a3c4eb223d9
wscatao <wscatao@gmail.com>
2020-04-10 12:15:01 -0300

    Requisito 2 e 3

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const myObj = { method: 'GET' };
 
 const sectionItem = document.querySelector('.items');
 
+
+// Funções
+
 function montarObj(json) {
   const arrResults = [];
   json.results.forEach((el) => {
@@ -57,23 +60,55 @@ function criaElementosNaTela(arr) {
   });
 }
 
+function cartItemClickListener(event) {
+  event.target.remove();
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+function montarObjCartItem(data) {
+  const objForCartItem = {
+    sku: data.id,
+    name: data.title,
+    salePrice: data.price,
+  };
+  const li = createCartItemElement(objForCartItem);
+  const ol = document.querySelector('.cart__items');
+  ol.appendChild(li);
+}
+
+const fetchItemPorID = (id) => {
+  const URL = `https://api.mercadolibre.com/items/${id}`;
+  fetch(URL)
+    .then(response => response.json())
+    .then(data => montarObjCartItem(data));
+};
+
+const coletarIDsDoElementoClicado = (event) => {
+  const acessarSection = event.target.parentNode;
+  const idFirstElement = acessarSection.firstChild.innerText;
+  fetchItemPorID(idFirstElement);
+};
+
+function queryButtons() {
+  const buttons = document.querySelectorAll('.item__add');
+  buttons.forEach((el) => {
+    el.addEventListener('click', coletarIDsDoElementoClicado);
+  });
+}
+
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
-// }
-
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
-
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
 // }
 
 fetch(API_URL, myObj)
   .then(response => response.json())
   .then(jsonResponse => montarObj(jsonResponse))
-  .then(arr => criaElementosNaTela(arr));
+  .then(arr => criaElementosNaTela(arr))
+  .then(queryButtons);
