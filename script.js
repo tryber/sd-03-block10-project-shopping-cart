8759a183287dc0c95248e394dac3c67d33d42ad6
DiegoRafaelAF <draf0314@gmail.com>
2020-04-10 17:19:22 -0300

    requisito 1 Listagem de Produtos

--- script.js
+++ script.js
@@ -36,8 +36,25 @@ function cartItemClickListener(event) {
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
-  li.className = 'cart__item';
+  li.className = 'item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+window.onload = function onload() {
+  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+    .then(resp => resp.json)
+    .then(json => {
+      json.results.forEach((product) => {
+        document.getElementsByClassName('item')[0]
+          .appendChild(createProductItemElement(
+            {
+              sku: product.id,
+              name: product.title,
+              image: product.thumbnail,
+            }
+          ))
+      });
+    })
+};
