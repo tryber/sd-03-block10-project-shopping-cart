efb4bc36e7a931d12d5b2156e08faee098fa54c8
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2020-04-11 10:23:07 -0300

    Atualização

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
+window.onload = function onload() { };
 const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
 const myObj = { method: 'GET' };
-// Pegar Elementos no HTML
+
 const sectionItem = document.querySelector('.items');
-// Funções
+
 function montarObj(json) {
   const arrResults = [];
   json.results.forEach((el) => {
     arrResults.push(el);
   });
+
   const arrProducts = [];
   arrResults.forEach((el) => {
     arrProducts.push({
@@ -16,6 +18,7 @@ function montarObj(json) {
       image: el.thumbnail,
     });
   });
+
   return arrProducts;
 }
 function createProductImageElement(imageSource) {
@@ -24,28 +27,33 @@ function createProductImageElement(imageSource) {
   img.src = imageSource;
   return img;
 }
+
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
 }
+
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
+
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+
   return section;
 }
+
 function criaElementosNaTela(arr) {
   arr.forEach((el) => {
     sectionItem.appendChild(createProductItemElement(el));
   });
 }
 
-function additemcart() {
+function cartItemClickListener(event){
   event.target.remove();
 }
 
@@ -53,9 +61,10 @@ function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', additemcart);
+  li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
 function montarObjCartItem(data) {
   const objForCartItem = {
     sku: data.id,
@@ -66,26 +75,29 @@ function montarObjCartItem(data) {
   const ol = document.querySelector('.cart__items');
   ol.appendChild(li);
 }
+
 const fetchItemPorID = (id) => {
-  const API_LINK = `https://api.mercadolibre.com/items/${id}`;
-  fetch(API_LINK)
+  const URL = `https://api.mercadolibre.com/items/${id}`;
+  fetch(URL)
     .then(response => response.json())
     .then(data => montarObjCartItem(data));
 };
+
 const coletarIDsDoElementoClicado = (event) => {
   const acessarSection = event.target.parentNode;
   const idFirstElement = acessarSection.firstChild.innerText;
   fetchItemPorID(idFirstElement);
 };
+
 function queryButtons() {
   const buttons = document.querySelectorAll('.item__add');
   buttons.forEach((el) => {
     el.addEventListener('click', coletarIDsDoElementoClicado);
-  });
-}
+    });
+  }
 
-fetch(API_URL, myObj)
-  .then(response => response.json())
-  .then(jsonResponse => montarObj(jsonResponse))
-  .then(arr => criaElementosNaTela(arr))
-  .then(queryButtons);
+  fetch(API_URL, myObj)
+    .then(response => response.json())
+    .then(jsonResponse => montarObj(jsonResponse))
+    .then(arr => criaElementosNaTela(arr))
+    .then(queryButtons);
