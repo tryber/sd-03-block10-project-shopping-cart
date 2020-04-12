127e00188bb773b7ee933243ef7d371140f0c818
potew <potew0@gmail.com>
2020-04-12 15:57:45 -0300

    Mudança de ordem e nomenclatura nas funções

--- script.js
+++ script.js
@@ -21,6 +21,42 @@ function loadingText(mode) {
   }
 }
 
+// Apaga o ítem do carrinho e recalcula o valor total dos ítens
+function cartItemClickListener(event) {
+  (event.target).parentNode.removeChild(event.target);
+  getTotalValue((event.target).innerText.match(/[^$]*$/), 'sub');
+  // Procura por tudo que vier depois do $, que corresponde ao preço do produto
+}
+
+// Usada para inserir as imagens nos respectivos produtos
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+}
+
+// Coloca o item selecionado na lista do carrinho
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+// Obtém os dados do produto selecionado na grade pela segunda URL (no topo)
+async function add2Cart({ sku }) {
+  await fetch(urlProduto(sku))
+    .then(resposta => resposta.json())
+    .catch(erro => alert('Erro na obtenção da lista', erro))
+    .then((respjson) => {
+      const paramProd = { sku: respjson.id, name: respjson.title, salePrice: respjson.price };
+      document.querySelector('.cart__items').appendChild(createCartItemElement(paramProd));
+      getTotalValue(paramProd.salePrice, 'add');
+    });
+}
+
 // Cria a grade com os resultados da busca inicial
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
@@ -29,13 +65,13 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   const addToCart = (createCustomElement('button', 'item__add', 'Adicionar ao carrinho'));
-  addToCart.addEventListener('click', () => adicionaProduto({ sku }));
+  addToCart.addEventListener('click', () => add2Cart({ sku }));
   section.appendChild(addToCart);
   return section;
 }
 
 // Gera a grade com os resultados da busca na primeira URL (no topo)
-async function buscaProdutos() {
+async function getProducts() {
   document.querySelector('.items').innerHTML = '';
   loadingText(true);
   const campoBusca = document.querySelector('#input-search').value;
@@ -52,52 +88,16 @@ async function buscaProdutos() {
     });
 }
 
-// Usada para inserir as imagens nos respectivos produtos
-function createProductImageElement(imageSource) {
-  const img = document.createElement('img');
-  img.className = 'item__image';
-  img.src = imageSource;
-  return img;
-}
-
-// Obtém os dados do produto selecionado na grade pela segunda URL (no topo)
-async function adicionaProduto ({ sku }) {
-  await fetch(urlProduto(sku))
-    .then(resposta => resposta.json())
-    .catch(erro => alert('Erro na obtenção da lista', erro))
-    .then((respjson) => {
-      const paramProd = { sku: respjson.id, name: respjson.title, salePrice: respjson.price };
-      document.querySelector('.cart__items').appendChild(createCartItemElement(paramProd));
-      calculaTotal(paramProd.salePrice, 'add');
-    });
-}
-
 // Faz a soma do total dos ítens do carrinho ao mesmo tempo que armazena a lista no localStorage
-async function calculaTotal(valor, operador) {
+async function getTotalValue(valor, operador) {
   const itensCarrinho = document.getElementsByClassName('cart__items')[0].innerHTML;
-  operador === 'add' ? valorCarrinho += valor : valorCarrinho -= valor;
+  operador === 'add' ? (valorCarrinho += valor) : (valorCarrinho -= valor);
   document.getElementsByClassName('total-price')[0].innerText = `Total: R$ ${valorCarrinho}`;
   localStorage.setItem('cartItems', itensCarrinho);
 }
 
-// Coloca o item selecionado na lista do carrinho
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
-
-// Apaga o ítem do carrinho e recalcula o valor total dos ítens
-function cartItemClickListener(event) {
-  (event.target).parentNode.removeChild(event.target);
-  calculaTotal((event.target).innerText.match(/[^$]*$/), 'sub');
-  // Procura por tudo que vier depois do $, que corresponde ao preço do produto
 }
 
 // Limpa o carrinho e o localStorage
@@ -116,8 +116,8 @@ window.onload = function onload() {
   if (itensCarrinho.childElementCount) {
     document.getElementsByTagName('ol')[0].childNodes.forEach((item) => {
       item.addEventListener('click', cartItemClickListener);
-      calculaTotal(parseFloat((item).innerText.match(/[^$]*$/)), 'add');
+      getTotalValue(parseFloat((item).innerText.match(/[^$]*$/)), 'add');
     });
   }
-  buscaProdutos('Computador');
+  getProducts('Computador');
 };
