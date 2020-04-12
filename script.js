39c9b990a944b5b5f90750cbfa5a936eac153f7c
Clayton Pereira <cpereiramt@gmail.com>
2020-04-12 16:43:35 -0400

    Salvando alteraçoes  para o requisito 4

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // const products = [];
 const items = document.querySelector('.items');
 const cartItems = document.querySelector('.cart__items');
-const arrayITensListCart = [];
+let arrayITensListCart = [];
+let itemsAdd = [];
 window.onload = function onload() {
   const itemsForReloadCart = localStorage.getItem('listItemsAdd');
-  const arrayITensListCart= JSON.parse(itemsForReloadCart);
+  arrayITensListCart = JSON.parse(itemsForReloadCart);
   console.log(arrayITensListCart);
   arrayITensListCart.forEach(element => this.loadJsonPerProduct(element));
 };
@@ -40,10 +41,10 @@ function getSkuFromProductItem(item) {
 function cartItemClickListener(event) {
   // coloque seu código aqui
   const indexRemoved = Array.from(event.currentTarget.parentNode.children).indexOf(event.target);
-  //itemsAdd = arrayITensListCart.splice(indexRemoved, 1);
+  // itemsAdd = arrayITensListCart.splice(indexRemoved, 1);
   console.log(itemsAdd.splice(indexRemoved, 1));
   event.currentTarget.parentNode.removeChild(event.target);
-  localStorage.setItem('listItemsAdd', JSON.stringify(itemsAdd))
+  localStorage.setItem('listItemsAdd', JSON.stringify(itemsAdd));
 }
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
@@ -55,21 +56,19 @@ function createCartItemElement({ id: sku, title: name, price: salePrice }) {
 }
 
 // Retorna um unico produto filtrado pelo id.
-let itemsAdd = [];
 
-function loadJsonPerProduct(id) {  
+function loadJsonPerProduct(id) {
   const URL = `https://api.mercadolibre.com/items/${id}`;
   fetch(URL, { method: 'get' })
   .then(resp => resp.json())
   .then((data) => {
     cartItems.appendChild(createCartItemElement(data));
-    itemsAdd.push(data.id);    
+    itemsAdd.push(data.id);
     localStorage.setItem('listItemsAdd', JSON.stringify(itemsAdd));
-  }) 
+  })
   .catch((err) => {
     console.error(err);
-  });
-  
+  });  
 }
 
 // Retorna todas a lista de produtos que será utilizada pela aplicação .
