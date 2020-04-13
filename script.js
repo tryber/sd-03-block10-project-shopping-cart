4831dff1b6520a9ca3a89a141f40c521d2ddc736
henrique <hbaetaleite@gmail.com>
2020-04-13 18:44:46 -0300

    CodeClimate v2

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ window.onload = function onload() {
       });
     });
 
-  const btnEmptyCart = document.getElementsByClassName('empty-cart')[0]
+  const btnEmptyCart = document.getElementsByClassName('empty-cart')[0];
   btnEmptyCart.addEventListener('click', () => {
     document.getElementsByClassName('cart__items')[0].innerHTML = '';
   })
@@ -38,12 +38,12 @@ function createProductItemElement({ sku, name, image }) {
 
   const btn = (createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
   btn.addEventListener('click', () => {
-     fetch(`https://api.mercadolibre.com/items/${sku}`)
-     .then(resp => resp.json())
-     .then((json) => {
-       const item = document.getElementsByClassName('cart__items')[0];
-       item.appendChild(createCartItemElement({ sku: json.id, name: json.title, salePrice: json.price }));
-     });
+      fetch(`https://api.mercadolibre.com/items/${sku}`)
+      .then(resp => resp.json())
+      .then((json) => {
+        const item = document.getElementsByClassName('cart__items')[0];
+        item.appendChild(createCartItemElement({ sku: json.id, name: json.title, salePrice: json.price }));
+      });
   });
   section.appendChild(btn);
   return section;
