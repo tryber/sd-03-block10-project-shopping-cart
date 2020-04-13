7f34b09dfab39baf3650e364cee4b78a6a0166bb
tamagoshii <estelaap_martins@hotmail.com>
2020-04-13 19:23:14 -0300

    validando primeiro requisito

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-window.onload = function onload() { };
+window.onload = function onload() {
+  const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  this.fetch(API_URL)
+    .then(aux => aux.json())
+    .then(obj => obj.results.forEach(test => document.getElementsByClassName('items')[0].appendChild(createProductItemElement({
+      sku: test.id,
+      name: test.title,
+      image: test.thumbnail,
+    }))));
+};
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
