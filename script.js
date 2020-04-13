60971b4d2fd5351ec58a2fbcbdc88e3ceb5fe76e
Lucas Franca <luc.rangel90@gmail.com>
2020-04-13 18:48:33 -0300

    tá difícil

--- script.js
+++ script.js
@@ -13,17 +13,20 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+const createProductItemElement = ({ sku, name, image }) => {
   const section = document.createElement('section');
   section.className = 'item';
 
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+  const botao = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  botao.addEventListener('click', () => {
+    addToCart({ sku });
+  });
+  section.appendChild(botao);
   return section;
-}
+};
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
