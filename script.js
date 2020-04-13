ee7bd493efce410ab949737f576c38d04a2b7086
Lucas Franca <luc.rangel90@gmail.com>
2020-04-13 18:28:17 -0300

    algumas alterações do primeiro requisito

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ function createProductImageElement(imageSource) {
   img.className = 'item__image';
   img.src = imageSource;
   return img;
-}
+};
 
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
-}
+};
 
 const createProductItemElement = ({ sku, name, image }) => {
   const section = document.createElement('section');
@@ -20,18 +20,25 @@ const createProductItemElement = ({ sku, name, image }) => {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+  const botao = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  botao.addEventListener('click', () => {
+    addToCart({ sku });
+  });
+  section.appendChild(botao);
+  
   return section;
 };
 
+const apiMercadoLivre = api => fetch(api).then(response => response.json());
+
+
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
+};
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
-}
+};
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
@@ -39,10 +46,10 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-}
+};
 
 window.onload = async () => {
-  await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+  await apiMercadoLivre('https://api.mercadolibre.com/sites/MLB/search?q=computador')
     .then((json) => {
       json.results.forEach((product) => {
         document.querySelector('items').appendChild(
