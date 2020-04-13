93cef9e28fedb913e82cec51ea33ef3929dc83bf
henrique <hbaetaleite@gmail.com>
2020-04-13 19:23:59 -0300

    CodeClimate v8

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function cartItemClickListener(event) {
+  event.target.remove();
+}
+
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
@@ -44,10 +48,6 @@ function createProductItemElement({ sku, name, image }) {
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  event.target.remove();
 }
 
 window.onload = function onload() {
