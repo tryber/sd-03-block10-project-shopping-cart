909a508719543b9f8b9f8f46ab5a3e9693830fa7
leonardodamasceno8 <leonardodamasceno8@users>
2020-04-13 16:07:49 -0300

    carrinho de compras requisito 1

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ function createCustomElement(element, className, innerText) {
 }
 
 function createProductItemElement({ sku, name, image }) {
-  
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -49,8 +48,7 @@ window.onload = async () => {
   .then((data) => {
     const results = data.results;
     results.forEach(({ id, title, thumbnail }) => {
-      console.log(id, title, thumbnail)
-      listaDeitens.appendChild(createProductItemElement({ sku: id, name: title, image: thumbnail }))
+      listaDeitens.appendChild(createProductItemElement({ sku: id, name: title, image: thumbnail }));
     });
     return data;
   })
