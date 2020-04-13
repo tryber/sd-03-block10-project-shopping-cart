50a9d1f873cbd25b198fdd7e6a8e6a82a02a37f5
Clayton Pereira <cpereiramt@gmail.com>
2020-04-12 21:35:21 -0400

    Salvando alteraçoes  para o requisito 4 (19)

--- script.js
+++ script.js
@@ -40,6 +40,20 @@ const itemsForReloadCart = localStorage.getItem('listItemsAdd');
 const itemsTemp = JSON.parse(itemsForReloadCart);
 itemsTemp.forEach(element => loadJsonPerProduct(element));
 
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+}
+
+function createCustomElement(element, className, innerText) {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+}
+
 function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -75,20 +89,6 @@ function loadJson() {
 window.onload = function onload() {
   loadJson();
 };
-
-function createProductImageElement(imageSource) {
-  const img = document.createElement('img');
-  img.className = 'item__image';
-  img.src = imageSource;
-  return img;
-}
-
-function createCustomElement(element, className, innerText) {
-  const e = document.createElement(element);
-  e.className = className;
-  e.innerText = innerText;
-  return e;
-}
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
