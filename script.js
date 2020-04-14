5b043866fab561598d980775e1ed13bdd2a6f760
Rhian Castro <rhi.castro@gmail.com>
2020-04-14 18:26:56 -0300

    Your heart's been aching but you're too shy to say it

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-window.onload = function onload() {};
 const apiSearchUrl = 'https://api.mercadolibre.com/sites/MLB/search?q=';
 const query = 'computador';
 
-function createProductImageElement(imageSource) {
+const createProductImageElement = imageSource => {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
-}
+};
 
-function createCustomElement(element, className, innerText) {
+const createCustomElement = (element, className, innerText) => {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
-}
+};
 
-function createProductItemElement({ sku, name, image }) {
+const createProductItemElement = ({ sku, name, image }) => {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -28,25 +27,25 @@ function createProductItemElement({ sku, name, image }) {
   );
 
   return section;
-}
+};
 
-function getSkuFromProductItem(item) {
+const getSkuFromProductItem = item => {
   return item.querySelector('span.item__sku').innerText;
-}
+};
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+const cartItemClickListener = event => {
+  document.querySelector('.cart__items').removeChild(event.target);
+};
 
-function createCartItemElement({ sku, name, salePrice }) {
+const createCartItemElement = ({ sku, name, salePrice }) => {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-}
+};
 
-window.onload = function onload() {
+const getProductData = async () => {
   fetch(apiSearchUrl + query)
     .then(data => data.json())
     .then(obj =>
@@ -60,4 +59,8 @@ window.onload = function onload() {
         ),
       ),
     );
+};
+
+window.onload = async function onload() {
+  await getProductData();
 };
