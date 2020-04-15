1e34f9241a8609472391619a7b656203a033465c
Hfreitas <hfreitas86@gmail.com>
2020-04-15 00:44:21 -0300

    Alterações para atender code climate

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ const getProductData = async () => {
   const API_URL = `https://api.mercadolibre.com/sites/MLB/search?q=${queryKey}`;
   await fetch(API_URL)
     .then(response => response.json())
-    .then((data) =>
+    .then(data =>
       data.results.forEach(({ id, title, thumbnail }) =>
         productsList.appendChild(
           createProductItemElement({ sku: id, name: title, image: thumbnail }),
