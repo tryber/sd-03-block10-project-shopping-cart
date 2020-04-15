6a03764e5574e40d68128f3087284f8b7972088a
luis.campos <gucampos845@gmail.com>
2020-04-15 14:56:14 -0300

    Correção codeClimate

--- script.js
+++ script.js
@@ -95,25 +95,27 @@ function load() {
   document.querySelector('.loading').innerHTML = 'loading...';
 }
 
-fetch(API_URL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    const a = document.querySelector('.items');
-    data.results.forEach((procura) => {
-      const all = { sku: procura.id, name: procura.title, image: procura.thumbnail };
-      const product = createProductItemElement(all);
-      a.appendChild(product);
-    });
-  })
-  .then(load)
-  .then(botaoAdd);
-const apagaAll = document.getElementById('empty-cart');
-apagaAll.addEventListener('click', function () {
-  const lista = document.querySelector('.cart__items');
-  lista.innerHTML = '';
-  newLocalStorage();
-  return lista;
-});
-recuperaLS();
+window.onload = function onload() {
+  fetch(API_URL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      const a = document.querySelector('.items');
+      data.results.forEach((procura) => {
+        const all = { sku: procura.id, name: procura.title, image: procura.thumbnail };
+        const product = createProductItemElement(all);
+        a.appendChild(product);
+      });
+    })
+    .then(load)
+    .then(botaoAdd);
+  const apagaAll = document.getElementById('empty-cart');
+  apagaAll.addEventListener('click', function () {
+    const lista = document.querySelector('.cart__items');
+    lista.innerHTML = '';
+    newLocalStorage();
+    return lista;
+  });
+  recuperaLS();
+}
