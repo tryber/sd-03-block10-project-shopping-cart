4706d6a5f4b4fa550e7f09789aa4fc852014a863
William Igor <williamigorsilva@gmail.com>
2020-04-16 19:43:56 -0300

    CodeCLimate

--- script.js
+++ script.js
@@ -3,23 +3,18 @@
 //   name: element.results.title,
 //   thumbnail: element.results.thumbnail,
 // }
+async function cartItemClickListener(event) {
+  await event.target.remove();
+  await localStorage.setItem('cart__items', document.getElementsByClassName('cart__items')[0].innerHTML);
+}
 
-window.onload = function onload() {
-  fetch(`https://api.mercadolibre.com/sites/MLB/search?q=computador`)
-    .then(respostaRecebida => respostaRecebida.json())
-    .then(essaRespostaBusca => essaRespostaBusca.results.forEach(produts => {
-      document.getElementsByClassName('items')[0].appendChild(createProductItemElement({
-        sku: produts.id,
-        name: produts.title,
-        image: produts.thumbnail,
-      }));
-      document.getElementsByClassName('cart__items')[0].innerHTML = localStorage.getItem('cart__items');
-      document.querySelectorAll('li').forEach(e => e.addEventListener('click', cartItemClickListener));
-      document.getElementsByClassName('empty-cart')[0].addEventListener('click', () => {
-        document.getElementsByClassName('cart__items')[0].innerHTML = '';
-      });
-    }));
-};
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -35,6 +30,10 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
+
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -45,8 +44,8 @@ function createProductItemElement({ sku, name, image }) {
   const btn = (createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
   btn.addEventListener('click', () => {
     fetch(`https://api.mercadolibre.com/items/${sku}`)
-      .then(resolve => resolve.json())
-      .then((produts => {
+      .then((resolve) => resolve.json())
+      .then(((produts) => {
         const item = document.getElementsByClassName('cart__items')[0];
         item.appendChild(createCartItemElement({
           sku: produts.id,
@@ -54,25 +53,25 @@ function createProductItemElement({ sku, name, image }) {
           salePrice: produts.price,
         }));
       }));
-    // localStorage.setItem('chave, document.getElementsByClassName('cart__items')[0].innerHTML);
+    localStorage.setItem('cart__items', document.getElementsByClassName('cart__items')[0].innerHTML);
   });
   section.appendChild(btn);
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
-async function cartItemClickListener(event) {
-  await event.target.remove();
-  await localStorage.setItem('cart__items', document.getElementsByClassName('cart__items')[0].innerHTML);
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+window.onload = function onload() {
+  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+    .then((respostaRecebida) => respostaRecebida.json())
+    .then((essaRespostaBusca) => essaRespostaBusca.results.forEach((produts) => {
+      document.getElementsByClassName('items')[0].appendChild(createProductItemElement({
+        sku: produts.id,
+        name: produts.title,
+        image: produts.thumbnail,
+      }));
+      document.getElementsByClassName('cart__items')[0].innerHTML = localStorage.getItem('cart__items');
+      document.querySelectorAll('li').forEach(e => e.addEventListener('click', cartItemClickListener));
+      document.getElementsByClassName('empty-cart')[0].addEventListener('click', () => {
+        document.getElementsByClassName('cart__items')[0].innerHTML = '';
+      });
+    }));
+};
