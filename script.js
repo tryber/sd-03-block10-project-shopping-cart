1fa6ac6cab02fa9a9cddefa5800e8cb5411b1203
Lucas Franca <luc.rangel90@gmail.com>
2020-04-17 18:52:51 -0300

    2 requisitos

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+ 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -12,13 +13,13 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
-  event.target.remove();
+async function cartItemClickListener(event) {
+  await event.target.remove();
+  await localStorage.setItem('cart__items', document.querySelector('.cart__items').innerHTML);
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -26,20 +27,10 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.className = 'item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
+
   return li;
 }
 
-
-// const cart = (data) => {
-//   let obj = {
-  //     sku: data.id,
-//     name: data.title,
-//     salePrice: data.price,
-//   };
-//   const li = createCartItemElement(obj);
-//   const ol = document.querySelector('.cart__items');
-//   ol.appendChild(li);
-// };
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -47,36 +38,36 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  const btnAddCart = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
-  btnAddCart.addEventListener('click', () => {
-    fetch(`https://api.mercadolibre.com/items/${sku}`)
-      .then(data => data.json())
-      .then((product) => {
-        const item = document.querySelector('.cart__items');
-        item.appendChild(createCartItemElement({
-          sku: product.id,
-          name: product.title,
-          salePrice: product.price,
-        }));
-      });
-  });
-  section.appendChild(btnAddCart);
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'))
+    .addEventListener('click', () => addItems(sku));
+
   return section;
 }
 
-window.onload = function onload() {
-  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
-  .then(data => data.json())
-  .then((json) => {
-    json.results.forEach((product) => {
-      document.querySelector('.items')
-        .appendChild(createProductItemElement(
-          {
-            sku: product.id,
-            name: product.title,
-            image: product.thumbnail,
-          },
-        ));
-    });
+const items = product => ({
+  sku: product.id,
+  name: product.title,
+  salePrice: product.price,
+  image: product.thumbnail,
+});
+
+addItems = async (sku) => {
+  await fetch(`https://api.mercadolibre.com/items/${sku}`)
+    .then(data => data.json())
+    .then(products => document.querySelector('.cart__items')
+      .appendChild(createCartItemElement(items(products))));
+  await localStorage.setItem('cart__items', document.getElementsByClassName('cart__items')[0].innerHTML);
+};
+
+window.onload = async function onload() {
+  await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+    .then(resp => resp.json())
+    .then(json => json.results.forEach(products => document.getElementsByClassName('items')[0]
+      .appendChild(createProductItemElement(items(products)))));
+  document.querySelector('.cart__items').innerHTML = localStorage.getItem('cart__items');
+  document.querySelectorAll('li').forEach(item => item.addEventListener('click', cartItemClickListener));
+  document.querySelector('.empty-cart')[0].addEventListener('click', () => {
+    document.querySelector('.cart__items').innerHTML = '';
+    localStorage.setItem('cart__items', document.querySelector('.cart__items').innerHTML);
   });
 };
