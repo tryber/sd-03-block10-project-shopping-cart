8e586acc09b5e1594cc2ec8bc9868d2af7db6486
PedroParreiras <pedro.bgparreiras@gmail.com>
2020-04-18 14:35:05 -0300

    segundo requisito

--- script.js
+++ script.js
@@ -14,6 +14,19 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+window.onload = function onload() {
+  const addItems = async () => {
+    const openApi = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=$computador');
+    const awaitApi = await openApi.json();
+    const itemsHTML = document.getElementsByClassName('items')[0];
+    awaitApi.results.forEach((e) => {
+      const values = { sku: e.id, name: e.title, image: e.thumbnail };
+      itemsHTML.appendChild(createProductItemElement(values));
+    });
+  };
+  addItems();
+};
+
 const addToCart = async (sku) => {
   const getApi = await fetch(`https://api.mercadolibre.com/items/${sku}`);
   const apiWait = await getApi.json();
@@ -39,19 +52,6 @@ function createProductItemElement({ sku, name, image }) {
 
   return section;
 }
-
-window.onload = function onload() {
-  const addItems = async () => {
-    const openApi = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=$computador');
-    const awaitApi = await openApi.json();
-    const itemsHTML = document.getElementsByClassName('items')[0];
-    awaitApi.results.forEach((e) => {
-      const values = { sku: e.id, name: e.title, image: e.thumbnail };
-      itemsHTML.appendChild(createProductItemElement(values));
-    });
-  };
-  addItems();
-};
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
