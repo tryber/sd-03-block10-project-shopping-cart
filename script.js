08e120d8bb5793af0e04bc2f465f0a9d5a1ce1a3
PedroParreiras <pedro.bgparreiras@gmail.com>
2020-04-18 13:57:56 -0300

    segundo requisito

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 window.onload = function onload() {};
 
 function createProductImageElement(imageSource) {
-  const img = document.createElement("img");
-  img.className = "item__image";
+  const img = document.createElement('img');
+  img.className = 'item__image';
   img.src = imageSource;
   return img;
 }
@@ -15,18 +15,18 @@ function createCustomElement(element, className, innerText) {
 }
 
 function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement("section");
-  section.className = "item";
+  const section = document.createElement('section');
+  section.className = 'item';
 
-  section.appendChild(createCustomElement("span", "item__sku", sku));
-  section.appendChild(createCustomElement("span", "item__title", name));
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   const buttonCart = createCustomElement(
-    "button",
-    "item__add",
-    "Adicionar ao carrinho!"
+    'button',
+    'item__add',
+    'Adicionar ao carrinho!'
   );
-  buttonCart.addEventListener("click", () => addToCart(sku));
+  buttonCart.addEventListener('click', () => addToCart(sku));
   section.appendChild(buttonCart);
 
   return section;
@@ -35,10 +35,10 @@ function createProductItemElement({ sku, name, image }) {
 window.onload = function onload() {
   const addItems = async () => {
     const openApi = await fetch(
-      "https://api.mercadolibre.com/sites/MLB/search?q=$computador"
+      'https://api.mercadolibre.com/sites/MLB/search?q=$computador'
     );
     const awaitApi = await openApi.json();
-    const itemsHTML = document.getElementsByClassName("items")[0];
+    const itemsHTML = document.getElementsByClassName('items')[0];
     awaitApi.results.forEach((e) => {
       const values = { sku: e.id, name: e.title, image: e.thumbnail };
       itemsHTML.appendChild(createProductItemElement(values));
@@ -48,16 +48,16 @@ window.onload = function onload() {
 };
 
 function getSkuFromProductItem(item) {
-  return item.querySelector("span.item__sku").innerText;
+  return item.querySelector('span.item__sku').innerText;
 }
 
 function cartItemClickListener(event) {}
 
 function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement("li");
-  li.className = "cart__item";
+  const li = document.createElement('li');
+  li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener("click", cartItemClickListener);
+  li.addEventListener('click', cartItemClickListener);
   return li;
 }
 
@@ -69,7 +69,7 @@ const addToCart = async (sku) => {
     name: apiWait.title,
     salePrice: apiWait.price,
   });
-  const cart = document.getElementsByClassName("cart__items")[0];
+  const cart = document.getElementsByClassName('cart__items')[0];
   cart.appendChild(carItens);
 };
 addToCart();
