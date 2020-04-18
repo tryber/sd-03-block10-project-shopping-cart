8ae54bb12f619e4e36b7ef3df0b46092caf9f9bb
fomes <contato.felipeluna@gmail.com>
2020-04-18 13:31:00 -0300

    code climate fix 13.30

--- script.js
+++ script.js
@@ -14,19 +14,6 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-  
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'))
-  .addEventListener('click', () => get(sku));
-  
-  return section;
-}
-
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
@@ -56,16 +43,29 @@ async function get(sku) {
     .then(e => cart[0].appendChild(e));
 }
 
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'))
+    .addEventListener('click', () => get(sku));
+
+  return section;
+}
+
 const itens = async () => {
   await fetch(url)
-  .then(response => response.json())
-  .then(data => data.results.forEach(e => document.getElementsByClassName('items')[0]
-    .appendChild(createProductItemElement({
-      sku: e.id,
-      name: e.title,
-      image: e.thumbnail,
+    .then(response => response.json())
+    .then(data => data.results.forEach(e => document.getElementsByClassName('items')[0]
+      .appendChild(createProductItemElement({
+        sku: e.id,
+        name: e.title,
+        image: e.thumbnail,
       }))));
-  };
+};
 
 window.onload = function onload() {
   itens();
