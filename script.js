33ace9f04a3538e7d24041d099085180434cdd06
André Ferreira <andrefpires1@gmail.com>
2020-04-19 13:25:48 -0300

    Code Climate #7

--- script.js
+++ script.js
@@ -55,11 +55,11 @@ function createProductItemElement({ sku, name, image }) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
+,function cartItemClickListener(event) {
   // coloque seu código aqui - Excluir item do carrinho
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+,function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
@@ -67,7 +67,7 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-function apiCreateItem() {
+,function apiCreateItem() {
 window.onload = function onload() {
   fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador', myObject)
   .then(response => response.json())
