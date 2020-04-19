7586c2d5b5bf308708a6afe893af269c92e3aca3
Mateus Moreira <ashuraw@hotmail.com>
2020-04-19 20:15:10 -0300

    v2

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+}
+
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
@@ -50,10 +54,6 @@ function createProductItemElement({ sku, name, image }) {
 
 async function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  // coloque seu código aqui
 }
 
 async function StartApi() {
