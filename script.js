79c36e5d105fdf5c0a831879a27d3a636f68446a
Mateus Moreira <ashuraw@hotmail.com>
2020-04-20 01:15:48 -0300

    v5

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function cartItemClickListener(event) {
   event.target.remove();
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+async function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
