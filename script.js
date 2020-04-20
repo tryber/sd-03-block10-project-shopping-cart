5a6b8c675ecb883c1d4c8498c05e4dafe3804ffd
tamagoshii <estelaap_martins@hotmail.com>
2020-04-20 08:17:12 -0300

    teste

--- script.js
+++ script.js
@@ -78,6 +78,7 @@ window.onload = async () => {
   document.querySelectorAll('li').forEach(inner => inner.addEventListener('click', () => cartItemClickListener(inner)));
   document.getElementsByClassName('empty-cart')[0].addEventListener('click', () => {
     document.getElementsByClassName('cart__items')[0].innerHTML = '';
+    await addTotal(atualiza());
   });
   await desativar();
   await addTotal();
