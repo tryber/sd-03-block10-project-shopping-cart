9d1fcbf8077e5d004f5d41495a48e6a8f235404d
renan.andreolle <renandreolle@hotmail.com>
2020-04-20 10:45:52 -0300

    Code climate de novo2

--- script.js
+++ script.js
@@ -11,6 +11,20 @@ function createCustomElement(element, className, innerText) {
   e.innerText = innerText;
   return e;
 }
+
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+  event.target.remove();
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
 // Dado um ID, a function adiciona o produto com esse ID no cart
 async function getId(itemID) {
   const apiButton = `https://api.mercadolibre.com/items/${itemID}`;
@@ -20,14 +34,6 @@ async function getId(itemID) {
   .then(element => document.querySelector('.cart__items').appendChild(createCartItemElement({ sku: element.id, name: element.title, salePrice: element.price }))));
 
   localStorage.setItem('último carrinho', document.querySelector('.cart__items').innerHTML);
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
 }
 
 function createProductItemElement({ sku, name, image }) {
@@ -47,10 +53,6 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-  event.target.remove();
-}
 
 // Remove o loading e carrega os itens em forma visual
 const URL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
