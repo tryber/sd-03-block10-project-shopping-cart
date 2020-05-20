e778c521fa0833354aabc3f26323a9227296b443
marcosjuniorclimaco <markojuniorclimako@gmail.com>
2020-05-20 18:53:35 -0300

    alt2-js add other functions

--- script.js
+++ script.js
@@ -12,6 +12,15 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+const priceUpdate = async () => {
+  const totalPrice = document.getElementsByClassName('total-price');
+  const cartItemsHTMLcollection = document.getElementsByClassName('cart__item');
+  const cartTotal = [...cartItemsHTMLcollection]
+    .map(element => element.innerText.match(/([0-9.]){1,}$/))
+    .reduce((accumulator, currentValue) => accumulator + parseFloat(currentValue), 0);
+  totalPrice[0].innerHTML = cartTotal;
+};
+
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -32,16 +41,22 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
+const addToCart = (sku) => {
+  fetch(`https://api.mercadolibre.com/items/${sku}`)
+    .then(response => response.json())
+    .then(data => ({ sku: data.id, name: data.title, salePrice: data.price }))
+    .then(product => createCartItemElement(product))
+    .then(object => document.querySelector('.cart__items').appendChild(object))
+    .then(() => localStorage.setItem('cartItems', document.querySelector('.cart__items').innerHTML))
+    .then(() => priceUpdate());
+};
+
 const createProductList = () => {
   const productArray = [];
   const itemsSection = document.querySelector('.items');
   fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
-    .then(response => response.json())
-    .then(data => data.results.forEach(product => productArray.push({
-      sku: product.id,
-      name: product.title,
-      image: product.thumbnail
-    })))
+    .then(response => response.json()).then(data => data.results.forEach(product =>
+      productArray.push({ sku: product.id, name: product.title, image: product.thumbnail })))
     .then(() => productArray.forEach(obj =>
       itemsSection.appendChild(createProductItemElement(obj))));
 };
